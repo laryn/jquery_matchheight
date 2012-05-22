e874734dead03d87c420d7e7fd2b9d96688f60c0
unknown <Natalie@Natalie-PC.(none)>
2012-05-21 21:45:24 -0400

    Fixed undefined variable in javascript, extra description after ajax

--- equalheights.js
+++ equalheights.js
@@ -2,9 +2,11 @@
 	Drupal.behaviors.equalHeightsModule = {
 	  attach: function (context, settings) {
 	  var eqClass = Drupal.settings.equalHeightsModule;
+    if (eqClass) {
 	  //var overflow = Drupal.settings.equalHeightsModule.overflow;
 	    $.each(eqClass, function(eqClass, setting) {
 		  $('.' + setting.class).equalHeights(setting.minheight, setting.maxheight).css('overflow', setting.overflow)});
 	  }
+    }
 	}
 })(jQuery);
